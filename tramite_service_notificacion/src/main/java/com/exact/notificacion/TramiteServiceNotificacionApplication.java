17eb1637cee0ce97a82152979c0b1c4fb626647f
e-oheredia <oheredia@exact.com.pe>
2020-03-05 12:28:59 -0500

    Creación del proyecto

---
+++ tramite_service_notificacion/src/main/java/com/exact/notificacion/TramiteServiceNotificacionApplication.java
@@ -0,0 +1,13 @@
+package com.exact.notificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramiteServiceNotificacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TramiteServiceNotificacionApplication.class, args);
+	}
+
+}
---
+++ tramite_service_notificacion/src/test/java/com/exact/notificacion/TramiteServiceNotificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exact.notificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramiteServiceNotificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
